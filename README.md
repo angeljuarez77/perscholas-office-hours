4fe095709289ab1737f4b4f23493ce86fecc6d35
Angel Juarez <angeljuarez729@gmail.com>
2020-03-07 22:24:14 -0500

    start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# perscholas-office-hours
+# perscholas office hours
+
+This will be a repository where I consolidate the files i use for all of the office hours I will have for the cohort.
---
+++ saturday-3-7-20/callbackVsPromises.js
@@ -0,0 +1,44 @@
+function add() {
+  return 1 + 2;
+}
+
+function sevenPlusThree(anotherFunction) {
+  return anotherFunction() + 7;
+}
+
+// the callback is the function passed as an argument
+let number = sevenPlusThree(add);
+console.log(number);
+
+// nested function
+function outside() {
+  console.log('i am from outside function');
+
+  function inside() {
+    return 'returned from inside';
+  }
+
+  const message = inside(); // 'returned from inside'
+  // message = 'returned from inside'
+  console.log(message);
+  // console.log('returned from inside')
+}
+
+// outside();
+
+// asynchronous js and promises
+// $.ajax({
+//   url: 'https://swapi.co/api/people/1',
+// });
+
+let promise = $.ajax({
+  url: 'https://swapi.co/api/people/1',
+});
+// this looks like it should run first but it doesn't
+// because it is a promise
+promise.then((data) => {
+  console.log(data.name); // Luke Skywalker
+})
+.catch((e) => console.error(e));
+
+console.log('my console log');
